9150146882f3cb6da16ba3aa7821120aa4b8f278
zachalam <zelphics@gmail.com>
2018-11-10 21:42:20 -0800

    ui elements add

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -1,38 +1,23 @@
 <template>
-  
+
   <div class="home">
     <hr />
     <div style="padding:20px;">&nbsp;</div>
-  <img alt="Vue logo" src="../assets/woman.png" style="border-radius:50%; width:120px; height:120px; ">
+    <img alt="Vue logo" src="../assets/woman.png" style="border-radius:50%; width:100px; height:100px; ">
     <step1 />
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/wizard/HelloWorld.vue'
-import step1 from '@/components/wizard/step1.vue'
+  // @ is an alias to /src
+  import HelloWorld from '@/components/wizard/HelloWorld.vue'
+  import step1 from '@/components/wizard/step1.vue'
 
-export default {
-  name: 'home',
-  components: {
-    HelloWorld,
-    step1
-  }
-}
-</script>
-
-<style lang="scss">
-  body {
-    background-color:#fbfbfb;
-    color:red;
-    
-  }
-    </style>
-
-<style scoped lang="scss">
-    body {
-      background-color:#fbfbfb;
-      color:red;
+  export default {
+    name: 'home',
+    components: {
+      HelloWorld,
+      step1
     }
-      </style>
+  }
+</script>
