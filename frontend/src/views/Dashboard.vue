988e93e3e1f720af3002e2c8d78cde0f99f998a4
Mário Silva <mmcs@outlook.pt>
2018-11-11 18:44:43 +0000

    style fixes

--- frontend/src/views/Dashboard.vue
+++ frontend/src/views/Dashboard.vue
@@ -88,7 +88,6 @@
                     <td>{{ props.item.item }}</td>
                     <td>{{ props.item.name }}</td>
                     <td>{{ props.item.value }}</td>
-                    <td>See Detail</td>
                   </template>
                 </v-data-table>
               </v-flex>
@@ -100,9 +99,18 @@
   </v-app>
 </template>
 
+<style>
+.v-list__tile__title {
+  font-size: 20px;
+}
+table.v-table tbody td {
+  font-size: 17px;
+}
+</style>
+
 <script>
-import { Doughnut } from 'vue-chartjs';
-import LineChart from '../components/LineChart.vue';
+import { Doughnut } from 'vue-chartjs'
+import LineChart from '../components/LineChart.vue'
 
 export default {
   components: {
@@ -112,19 +120,19 @@ export default {
 
   async mounted() {
     var data = [this.marketCreatorEquity]
-      var bgColor = [this.marketCreatorBgColor]
+    var bgColor = [this.marketCreatorBgColor]
 
-      for (var i = 0; i < this.nrStakeHolder; i++) {
-      data.push((1 / this.nrStakeHolder) * 100)
-        var greyFragment = (40 + i * 2).toString(16)
-        bgColor.push('#' + greyFragment + greyFragment + greyFragment)
-      }
+    for (var i = 0; i < this.nrStakeHolder; i++) {
+      data.push((1 / this.nrStakeHolder) * 50)
+      var greyFragment = (40 + i * 2).toString(16)
+      bgColor.push('#' + greyFragment + greyFragment + greyFragment)
+    }
 
     var labels = ['accounta']
 
-      console.log(this.$refs)
+    console.log(this.$refs)
 
-      this.$refs.equityDoughnut.renderChart({
+    this.$refs.equityDoughnut.renderChart({
       datasets: [
         {
           data: data,
@@ -133,13 +141,13 @@ export default {
       ],
       labels: labels
     })
-    },
+  },
 
   data: () => ({
     drawer: true,
-    marketCreatorEquity: 50,
+    marketCreatorEquity: 51,
     marketCreatorBgColor: '#fda53a',
-    nrStakeHolder: 50,
+    nrStakeHolder: 3,
     lineChart1Data: {
       datasets: [
         {
@@ -205,12 +213,6 @@ export default {
         align: 'left',
         sortable: false,
         value: 'value'
-      },
-      {
-        text: 'Detail',
-        align: 'left',
-        sortable: false,
-        value: 'detail'
       }
     ],
     latestSells: [
@@ -229,13 +231,13 @@ export default {
 
   methods: {
     randomColor() {
-      var letters = '0123456789ABCDEF';
-      var color = '#';
+      var letters = '0123456789ABCDEF'
+      var color = '#'
       for (var i = 0; i < 6; i++) {
         color += letters[Math.floor(Math.random() * 16)]
-        }
-      return color
       }
+      return color
+    }
   }
 }
 </script>
